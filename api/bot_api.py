9a3517bedf2099fd61cc662853e7b871476bd0b3
Andrey Prokofiev <andreyprokofiev@macbp.local>
2024-11-18 08:21:22 +0300

    new method added

--- api/bot_api.py
+++ api/bot_api.py
@@ -1,17 +1,23 @@
 import uvicorn 
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 
 app=FastAPI()
 
+# @app.get("/")
+# def root():
+#     return {"message": "Hello World"}
+
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    data = {"message": "Hello METANIT.COM"}
+    json_data = jsonable_encoder(data)
+    return JSONResponse(content=json_data)
 
-
-# if __name__ == '__main__':
-#     uvicorn.run(app,
-#                 host='127.0.0.1',
-#                 port=80)
+@app.post('/user_mess')
+def user_mess():
+    pass
 
 
 if __name__ == "__main__":
